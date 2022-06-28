f1ff78381e0ce1bdf8098340a8f3cbcc05a4d071
robertoweb <roberto.alonso.gandia@gmail.com>
2022-06-28 14:42:55 +0200

    strapi-server basic until 41

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '93a134ba52ab13e0dd730a00c43aaa9e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dceeb865-fda7-4d0c-a1b7-d9cc2d7ead41'
+};
